c72018002c2cb6117c978c1f40c1975978018378
Sai Pc <schand31@asu.edu>
2015-12-26 21:05:03 +0530

    Minor refactor

--- add_comments.rb
+++ add_comments.rb
@@ -1,12 +1,14 @@
 module AddComments
   def comments
-    if @comments
-      # already exists, so return it
-      @comments
-    else
-      # create new array
-      @comments = []
-    end
+    # if @comments
+    #   # already exists, so return it
+    #   @comments
+    # else
+    #   # create new array
+    #   @comments = []
+    # end
+    # shorter and concise
+    @comments = @comments || []
   end
 
   def add_comments(comment)
